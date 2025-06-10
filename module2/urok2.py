4f74add5b0615bd69e4c5d0cabe7a7f996e83382
qwarqus <artemdiduh64@gmail.com>
2025-06-10 19:02:14 +0300

    дз

---
+++ module2/urok2.py
@@ -0,0 +1,29 @@
+from logging.config import stopListening
+
+phonenumber = {}
+print("1. Додати контакт")
+print("2. Пошук контакту")
+print("3. Видалити контакт")
+
+
+answer = input("Введіть команду: ")
+if answer == "1":
+    key = input("Input name")
+    value = input("Input number")
+    phonenumber[key] = value
+    print(f'контакт: {key} додано')
+    print(phonenumber)
+
+
+if answer == "2":
+    # get()
+    key = input("Введіть ім'я для пошуку: ")
+    print(f'{key}: {phonenumber.get(key, "контакт знайдено")}')
+
+if answer == "3":
+    key = input("Введіть ім'я для видалення: ")
+    removed = phonenumber.pop(key)
+    if removed:
+        print(f'Контакт {key} видалено.')
+    else:
+        print("Контакт не знайдено.")
